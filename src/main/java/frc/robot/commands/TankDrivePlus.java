e1c06901b62e6ee3edde40a664cf0fb40a05f546
Braden Mosier <bmosi77@tscstudents.net>
2024-07-25 09:46:07 -0400

    why is it right stick and then moved it to left

--- src/main/java/frc/robot/commands/TankDrivePlus.java
+++ src/main/java/frc/robot/commands/TankDrivePlus.java
@@ -36,8 +36,8 @@ public class TankDrivePlus extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    drivetrain.setLeftPower((controller.getRightStickY() - controller.getRightStickX()) * Constants.drivetrainSpeed);
-    drivetrain.setRightPower((controller.getRightStickY() + controller.getRightStickX()) * Constants.drivetrainSpeed);
+    drivetrain.setLeftPower((controller.getLeftStickY() - controller.getLeftStickX()) * Constants.drivetrainSpeed);// why was this right stick Braden
+    drivetrain.setRightPower((controller.getLeftStickY() + controller.getLeftStickX()) * Constants.drivetrainSpeed);
     // drivetrain.setLeftPower((controller.getLeftStickY()));
     // drivetrain.setRightPower((controller.getRightStickY()));
     intake.setPower(controller.getLeftTriggerAxis());
